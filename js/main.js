a3c1c0bbd6cf4afc62f962a150c104c7664d88a3
Your Name <you@example.com>
2016-04-07 20:40:55 +0800

    First commit to gh-pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b ";
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b";
 
 function getData(cb) {
   $.ajax({
