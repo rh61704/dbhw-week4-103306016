c866cf050648e566e5b1761e7acd83b6280c6c2e
Your Name <you@example.com>
2016-04-07 20:12:45 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b ";
+
+function getData(cb) {
+  $.ajax({
+    url : API_PATH,
+    method : "GET",
+    success : function(data) {
+      cb(null, data);
+    },
+    error : function(err) {
+      cb(err);
+    }
+  });
+}
